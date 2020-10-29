7d2dba8cf6c1183711b0e1fa0814aabbc6b863b5
Yungi <zoshs27@gmail.com>
2020-10-29 16:56:12 +0900

    Introduction Update

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,21 @@
-print("hello, world")
+## Machine Learning Tutorial for Beginners.
+# 사실상 Analyzing Model에 관한 실습 내용이다. 
+# https://www.kaggle.com/kanncaa1/machine-learning-tutorial-for-beginners/
+
+# Machine Learning
+# A. Supervised Learning
+#    a. EDA(Exploratory Data Analysis)
+#    b. K-Nearest Neighbors(KNN)
+#    c. Regression
+#    d. Cross Validation(CV)
+#    e. ROC Curve
+#    f. Hyperparameter Tuning
+#    g. Pre-processing Data
+#
+# B. Un-supervised Learning
+#    a. K-means Clustering
+#    b. Evaluation of Clustering
+#    c. Standardization
+#    d. Hierachy
+#    e. T-Distributed Stochastic Neighbor Embedding(T-SNE)
+#    f. Principle Component Analysis(PCA; 주성분 분석)
