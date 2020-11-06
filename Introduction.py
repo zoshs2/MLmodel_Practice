32c3887a68b7138b7a6f274afa5d007b34aedbd9
Yungi <zoshs27@gmail.com>
2020-11-06 22:29:06 +0900

    Introduction Update

--- Introduction.py
+++ Introduction.py
@@ -6,16 +6,18 @@
 # A. Supervised Learning
 #    a. EDA(Exploratory Data Analysis)
 #    b. K-Nearest Neighbors(KNN)
-#    c. Regression
+#    c. Regression (Ridge(L2) & Lasso(L1) Regression)
 #    d. Cross Validation(CV)
-#    e. ROC Curve
-#    f. Hyperparameter Tuning
-#    g. Pre-processing Data
+#    e. ROC Curve (AUC; Area under the ROC Curve)
+#    f. Hyperparameter Tuning (적절한 hyperparameter 선택기법)
+#    g. Pre-processing Data <- 건너뜀
 #
+# << Chapter2.py >>
 # B. Un-supervised Learning
-#    a. K-means Clustering
-#    b. Evaluation of Clustering
-#    c. Standardization
-#    d. Hierachy
-#    e. T-Distributed Stochastic Neighbor Embedding(T-SNE)
+#    a. K-means Clustering 
+#    b. Evaluation of Clustering 
+#       - (feat. Cross Tabulation Table, Inertia Value(=KMeans Cluster에서 적정 k값을 찾는 법))
+#    c. Standardization (feat. StandardScaler, make_pipeline)
+#    d. Hierachy <- 건너뜀
+#    e. T-Distributed Stochastic Neighbor Embedding(T-SNE) <- 건너뜀
 #    f. Principle Component Analysis(PCA; 주성분 분석)
